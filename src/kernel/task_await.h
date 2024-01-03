d0f510c74eeb0c1b284b3b6af746442e1f4c5fe7
gong <gong@qq.com>
2024-01-03 02:13:22 +0000

    优化任务状态逻辑

--- src/kernel/task_await.h
+++ src/kernel/task_await.h
@@ -7,9 +7,16 @@
 namespace galay
 {
     template<Request REQ ,Response RESP , typename RESULT>
-    class Co_Task:public Task_Base<REQ,RESP>,public Coroutine<RESULT>
+    class Co_Task_Base:public Task_Base<REQ,RESP>,public Coroutine<RESULT>
     {
     public:
+        using ptr = std::shared_ptr<Co_Task_Base>;
+
+        virtual void set_co_handle(std::coroutine_handle<> handle)
+        {
+            this->m_handle = handle;
+        }
+
         int exec() override
         {
             this->m_handle.resume();
