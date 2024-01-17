ea6c7231af9526edb3af18026993a3327cf924a2
gong <gong@qq.com>
2024-01-17 06:27:33 +0000

    新增openssl的 sha256和md5封装
    Former-commit-id: fc10731e0da933f7dacc2e086620bbbf83c60e96 [formerly 3b5a92e9ed60bb9e2ee0db83c31b6eb3b7159748]
    Former-commit-id: 1daac8474139f265dac879207a6d0555d29680ce

--- test/test_timer.cc
+++ test/test_timer.cc
@@ -1,14 +1,34 @@
 #include "../src/factory/factory.h"
+#include "../src/util/md5.h"
+#include "../src/util/sha256.h"
+#include <fstream>
+#include <signal.h>
 
 using namespace galay;
 
+void sig_handle(int sig)
+{
+
+}
+
+
 int main()
 {
-    auto scheduler = Scheduler_Factory::create_scheduler(IO_EPOLL,1024,-1);
+    signal(SIGINT,sig_handle);
+    auto scheduler = Scheduler_Factory::create_scheduler(IO_EPOLL, 1024, -1);
+    scheduler->get_timer_manager()->add_timer(Timer_Factory::create_timer(500, 1, []() {
+        std::ifstream in("/home/gong/projects/galay/1.txt");
+        if(in.fail()){
+            std::cout<<"fail\n";
+        }
+        char buffer[1024] = {0};
+        in.read(buffer,1024);
+        int byte = in.gcount();
+        std::string str(buffer,byte);
+        std::cout<<Md5Util::md5_encode(str)<<'\n';
+        std::cout << Sha256Util::sha256_encode(str) << '\n';
 
-    scheduler->get_timer_manager()->add_timer(Timer_Factory::create_timer(5000, 5, []()
-                                                                          { std::cout << "调用\n"; }));
-    
+    }));
 
     scheduler->start();
     return 0;
