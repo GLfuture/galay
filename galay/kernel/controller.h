db9a8dc37b23730863a8002ec23173e40d82f4ce
GLfuture <117423437+GLfuture@users.noreply.github.com>
2024-05-28 17:54:06 +0800

    Update controller.h
    增加Done接口，对内提供SetWaitGroup接口
    
    Former-commit-id: 4ad433d71e13e3ae4f45d556f0db7a96c220b471
    Former-commit-id: 099f4a1b33a3efe751627a48f5b22756b52c952d [formerly d7827b41dc745b19d3185982c910b536dc752261]
    Former-commit-id: 176af30e1b075257d9a5d2c8ad4af6564a35ec53

--- galay/kernel/controller.h
+++ galay/kernel/controller.h
@@ -2,18 +2,18 @@
 #define GALAY_CONTROLLER_H
 #include "coroutine.h"
 #include "awaiter.h"
+#include "waitgroup.h"
 #include <functional>
 #include <any>
 
 namespace galay {
     class Timer;
     class GY_IOScheduler;
-    class GY_UnaryStrategy;
     class GY_Connector;
     
     class GY_Controller: public std::enable_shared_from_this<GY_Controller>
     {
-        friend class GY_UnaryStrategy;
+        friend class GY_Connector;
     public:
         using ptr = ::std::shared_ptr<GY_Controller>;
         using wptr = ::std::weak_ptr<GY_Controller>;
@@ -26,10 +26,15 @@ namespace galay {
         void PushResponse(protocol::GY_TcpResponse::ptr response);
         ::std::any&& GetContext();
         ::std::shared_ptr<Timer> AddTimer(uint64_t during, uint32_t exec_times,::std::function<::std::any()> &&func);
-
+        
+        //协程结束时必须调用
+        void Done();
+    protected:
+        void SetWaitGroup(WaitGroup* waitgroup);
     private:
         ::std::weak_ptr<GY_Connector> m_connector;
         bool m_close;
+        WaitGroup* m_group;
     };
 }
 
