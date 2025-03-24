64de582371137b4728f3cc3e30e1bb858469e311
gong <gzj17607270093@163.com>
2025-03-24 11:46:25 +0000

    修复demo

--- tutorial/tutorial_etcd_register_client.cc
+++ tutorial/tutorial_etcd_register_client.cc
@@ -3,7 +3,7 @@
 
 galay::Coroutine<void> func(galay::RoutineCtx ctx)
 {
-    galay::etcd::EtcdClient client("http://127.0.0.1:2379", 0);
+    galay::etcd::EtcdClient client("http://127.0.0.1:2379");
     bool res = co_await client.RegisterService<void>("/app/api/login/node","127.0.0.1:7070");
     auto resp = client.GetResponse();
     std::cout << std::boolalpha << resp.Success() << std::endl;
